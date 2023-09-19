3ec687727de352f3396c77fe5924b05db8a53a1b
Shahin Sheikh <shahinsheikh.nrl@gmail.com>
2023-09-19 19:09:55 +0600

    Product CRUD test on service done

--- test/unit/product/crudProduct.test.js
+++ test/unit/product/crudProduct.test.js
@@ -32,7 +32,7 @@ describe("Product CRUD Test", () => {
     product: null,
   };
 
-  test("Crate Product Test", async () => {
+  test("Create Product test", async () => {
     const user = await UserService.create(userData);
     categoryData.user = user._id;
     const category = await CategoryService.create(categoryData);
@@ -58,5 +58,93 @@ describe("Product CRUD Test", () => {
 
     expect(product).toHaveProperty("_id");
     expect(updatedProductImage).toHaveProperty("modifiedCount", 1);
+  });
+
+  test("Update Product test", async () => {
+    const user = await UserService.create(userData);
+    categoryData.user = user._id;
+    const category = await CategoryService.create(categoryData);
+    const productImage = await ProductImageService.create(productImageData);
+    const data = {
+      title: "title",
+      slug: "slug",
+      description: "description",
+      productImage: [productImage._id],
+      published: true,
+      updated_at: new Date(),
+      user: user._id,
+      categories: [category._id],
+    };
+
+    const product = await ProductService.create(data);
+    const updatedProductImage = await ProductImageService.update(
+      productImage._id,
+      {
+        product: product._id,
+      }
+    );
+
+    data["title"] = "Title updated";
+
+    const result = await ProductService.update(product._id, data);
+
+    const updatedProduct = await ProductService.find(product._id);
+
+    expect(updatedProduct).toHaveProperty("title", data.title);
+    expect(result).toHaveProperty("modifiedCount", 1);
+    expect(updatedProductImage).toHaveProperty("modifiedCount", 1);
+  });
+
+  test("Delete Product test", async () => {
+    const user = await UserService.create(userData);
+    categoryData.user = user._id;
+    const category = await CategoryService.create(categoryData);
+    const productImage = await ProductImageService.create(productImageData);
+    const data = {
+      title: "title",
+      slug: "slug",
+      description: "description",
+      productImage: [productImage._id],
+      published: true,
+      updated_at: new Date(),
+      user: user._id,
+      categories: [category._id],
+    };
+
+    const product = await ProductService.create(data);
+
+    const deletedProduct = await ProductService.delete(product._id);
+    const deletedImage = await ProductImageService.delete(productImage._id);
+    const deletedCategory = await CategoryService.delete(category._id);
+    const deletedUser = await UserService.delete(user._id);
+
+    expect(deletedProduct).toHaveProperty("deletedCount", 1);
+    expect(deletedImage).toHaveProperty("deletedCount", 1);
+    expect(deletedCategory).toHaveProperty("deletedCount", 1);
+    expect(deletedUser).toHaveProperty("deletedCount", 1);
+  });
+  
+  test("Find Product test", async () => {
+    const user = await UserService.create(userData);
+    categoryData.user = user._id;
+    const category = await CategoryService.create(categoryData);
+    const productImage = await ProductImageService.create(productImageData);
+    const data = {
+      title: "title",
+      slug: "slug",
+      description: "description",
+      productImage: [productImage._id],
+      published: true,
+      updated_at: new Date(),
+      user: user._id,
+      categories: [category._id],
+    };
+
+    const product = await ProductService.create(data);
+
+    const result = await ProductService.find(product._id);
+
+    expect(result).toHaveProperty('_id')
+    
   });
 });
